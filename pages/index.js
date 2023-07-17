2794743107d06db38d4e369ec3745b6ca9e09056
manaomimasha <manaomi.masha@gmail.com>
2023-07-17 01:15:21 -0300

    sdsds

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
       <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
       <p className="text-2xl my-10">
-        Elige y personaliza tu pedido a continuación
+        Elige y personaliza tu pedido a continuación:
       </p>
 
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
